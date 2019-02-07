0d9ed08ed5aec6321cc51c52bee7b7fcddc9a40a
ehiggins98 <ericdhiggins@gmail.com>
2019-02-07 11:58:54 -0600

    Comments and README

---
+++ README.md
@@ -0,0 +1,8 @@
+# HackKU Meme Maker
+This repository contains the frontend code for our Beginners' Workshop at HackKU. To download the code, run:
+
+```
+git clone https://github.com/harryl6798/hackKU2019_demo
+```
+
+You can simply open the HTML file, and the website will work.
--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+/* 
+* Sets the width of the top and bottom text to the width of the image. This is simply so the text doesn't overflow
+* outside of the image if you enter more than one line of text.
+*
+* Note that we have to wait for window.onload because, when this script is called in the HTML, the image won't have
+* loaded yet.
+*/
 window.onload = function() {
+    // This can't be document.querySelector('img').style.width because that gets the width we set, not the display width
+    // It would therefore be 0, since we don't set the width in the CSS.
+    // Also, note the difference from C++. Here we just use "let" to declare all variables, instead of specifying a type.
     let width = document.querySelector('img').getBoundingClientRect().width + "px";
 
-    document.getElementById("center-top").style.width = width;
-    document.getElementById("center-bottom").style.width = width;
+    document.getElementById("top-text").style.width = width;
+    document.getElementById("bottom-text").style.width = width;
 }
 
+/*
+* Copies the text from the text boxes to the top and bottom text on the meme.
+*/
 function displayResult() {
-    document.getElementById("center-top").innerHTML = document.getElementById('textbox1').value;
-    document.getElementById("center-bottom").innerHTML = document.getElementById('textbox2').value;
+    // innerHTML is the stuff that appears inside the <div></div> tags.
+    // For example, if the HTML reads <div>asdf</div> then "asdf" is the innerHTML.
+    document.getElementById("top-text").innerHTML = document.getElementById('textbox1').value;
+    document.getElementById("bottom-text").innerHTML = document.getElementById('textbox2').value;
 }
