942f9ad4b93e82d043a9809e7870c878c361e9f1
ehiggins98 <ericdhiggins@gmail.com>
2019-02-07 11:31:35 -0600

    Minor improvements

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-document.onload = function() {
-    document.querySelector("img").onload = function() {
-        let width = document.querySelector('img').getBoundingClientRect().width + "px";
-    
-        document.getElementById("center-bottom").style.width = width;
-        document.getElementById("center-bottom").style.width = width;
-    }
+window.onload = function() {
+    let width = document.querySelector('img').getBoundingClientRect().width + "px";
+
+    document.getElementById("center-top").style.width = width;
+    document.getElementById("center-bottom").style.width = width;
 }
 
 function displayResult() {
